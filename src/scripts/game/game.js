5c7cdec6d85019930de2490ff3c4c80805177422
Norman (22194) <52394592+RedBlazerFlame@users.noreply.github.com>
2022-05-01 17:02:27 +0800

    The game page now redirects to the home page if the user has not gone through the introduction step yet

--- src/scripts/game/game.js
+++ src/scripts/game/game.js
@@ -1,9 +1,15 @@
 // Imports
 import { game } from "../Game.js";
-// Start the Event Dispatcher
-game.dispatcher.activate();
-// Dispatch the Current Event that is in the game store
-game.dispatcher.dispatch({
-    event: game.store.data.event,
-    countVisit: false,
-});
+// If the user hasn't created a username yet, redirect them to the main menu
+if (game.store.data.username === null) {
+    window.location.replace("/");
+}
+else {
+    // Start the Event Dispatcher
+    game.dispatcher.activate();
+    // Dispatch the Current Event that is in the game store
+    game.dispatcher.dispatch({
+        event: game.store.data.event,
+        countVisit: false,
+    });
+}
--- src/scripts/game/game.ts
+++ src/scripts/game/game.ts
@@ -1,11 +1,16 @@
 // Imports
 import { game } from "../Game.js";
 
-// Start the Event Dispatcher
-game.dispatcher.activate();
+// If the user hasn't created a username yet, redirect them to the main menu
+if (game.store.data.username === null) {
+    window.location.replace("/");
+} else {
+    // Start the Event Dispatcher
+    game.dispatcher.activate();
 
-// Dispatch the Current Event that is in the game store
-game.dispatcher.dispatch({
-    event: game.store.data.event,
-    countVisit: false,
-});
+    // Dispatch the Current Event that is in the game store
+    game.dispatcher.dispatch({
+        event: game.store.data.event,
+        countVisit: false,
+    });
+}
